4dc0b18d5a8ea94dca886fb6b91aa0612b87412f
Marcel Kucharik <marcel@tbi.univie.ac.at>
2015-12-18 15:13:31 +0100

    Just cosmetic changes of move_set

--- move_set_inside.c
+++ move_set_inside.c
@@ -5,12 +5,12 @@
 #include <limits.h>
 #include <time.h>
 
-#include "move_set_inside.h"
-
 #include "pair_mat.h"
 #include "fold_vars.h"
 #include "fold.h"
 #include "utils.h"
+
+#include "move_set.h"
 
 /* maximum degeneracy value - if degeneracy is greater than this, program segfaults */
 #define MAX_DEGEN 100
@@ -20,7 +20,7 @@
 #define true 1
 #define false 0
 
-int deal_deg = 1; // do we deal with the degeneracy?
+int deal_deg = 1; /* do we deal with the degeneracy? */
 
 void degeneracy_handling(int degeneracy)
 {
@@ -106,9 +106,6 @@ PRIVATE int     move_rset(Encoded *Enc, struct_en *str);
 
 PRIVATE int
 compare(short *lhs, short *rhs){
-
-  //printf("%d ", (int)lhs[0]);
-
   int i=1;
   char l=0,r=0;
   while (i<=lhs[0]) {
@@ -220,7 +217,7 @@ update_deepest(Encoded *Enc, struct_en *str, struct_en *min){
   if (Enc->funct) {
     int end = Enc->funct(str, min);
 
-    // undo moves
+    /* undo moves */
     if (Enc->bp_left2!=0) do_move(str->structure, -Enc->bp_left2, -Enc->bp_right2);
     do_move(str->structure, -Enc->bp_left, -Enc->bp_right);
     str->energy = last_en;
@@ -271,7 +268,6 @@ update_deepest(Encoded *Enc, struct_en *str, struct_en *min){
     }
 
     if (!found) {
-      //print_stren(stderr, str); // fprintf(stderr, " %6.2f\n", str->energy);
       Enc->unprocessed[Enc->end_unpr]=allocopy(str->structure);
       Enc->end_unpr++;
     }
@@ -316,7 +312,7 @@ deletions(Encoded *Enc, struct_en *str, struct_en *minim){
 
         /* check if we are in noLP space */
         if (!inside_pair && !outside_pair) {
-          fprintf(stderr, "WRONG!!!! %d %d\n", i, pt[i]);
+          fprintf(stderr, "WARNING: Lonely base pair when --noLP specified!!!! %d %d\n", i, pt[i]);
         }
 
         /* and make a delete or double delete if necessary, double delete do only from one side*/
@@ -358,7 +354,7 @@ compat(char a, char b){
   return false;
 }
 
-/* try insert base pair (i,j)*/
+/* try insert base pair (i,j) */
 PRIVATE inline bool
 try_insert(const short *pt, const char *seq, int i, int j){
 
@@ -366,14 +362,14 @@ try_insert(const short *pt, const char *seq, int i, int j){
   return (j-i>MINGAP && pt[j]==0 && pt[i]==0 && compat(seq[i-1], seq[j-1]));
 }
 
-// try insert base pair (i,j)
+/* try insert base pair (i,j) */
 PRIVATE inline bool
 try_insert_seq(const char *seq, int i, int j){
   if (i<=0 || j<=0) return false;
   return (j-i>MINGAP && compat(seq[i-1], seq[j-1]));
 }
 
-/* insertions move set*/
+/* insertions move set */
 PRIVATE int
 insertions(Encoded *Enc, struct_en *str, struct_en *minim){
 
@@ -412,7 +408,7 @@ insertions(Encoded *Enc, struct_en *str, struct_en *minim){
             }
           }
 
-          // finally insert:
+          /* finally insert: */
           cnt += update_deepest(Enc, str, minim);
           /* in case useFirst is on and structure is found, end*/
           if (Enc->first && cnt > 0) return cnt;
@@ -625,20 +621,17 @@ construct_moves(Encoded *Enc, short *structure){
       Enc->moves_from[Enc->num_moves]=-i;
       Enc->moves_to[Enc->num_moves]=-structure[i];
       Enc->num_moves++;
-      //fprintf(stderr, "add  d(%d, %d)\n", i, str.structure[i]);
     } else {
       int j;
       for (j=i+1; j<=structure[0]; j++) {
-        //fprintf(stderr, "check (%d, %d)\n", i, j);
         if (structure[j]==0) {
           if (try_insert_seq(Enc->seq,i,j)) {
             Enc->moves_from[Enc->num_moves]=i;
             Enc->moves_to[Enc->num_moves]=j;
             Enc->num_moves++;
-            //fprintf(stderr, "add  i(%d, %d)\n", i, j);
             continue;
           }
-        } else if (structure[j]>j) { // '('
+        } else if (structure[j]>j) { /* '(' */
           j = structure[j];
         } else break;
       }
@@ -673,7 +666,7 @@ move_rset(Encoded *Enc, struct_en *str){
 
   if (Enc->verbose_lvl>0) { fprintf(stderr, "  start of MR:\n  "); print_str(stderr, str->structure); fprintf(stderr, " %d\n\n", str->energy); }
 
-  // construct and permute possible moves
+  /* construct and permute possible moves */
   construct_moves(Enc, str->structure);
 
   /* find first lower one*/
@@ -784,26 +777,26 @@ move_gradient(char *string,
   enc.s0 = s;
   enc.s1 = s1;
 
-  /* moves*/
+  /* moves */
   enc.bp_left=0;
   enc.bp_right=0;
   enc.bp_left2=0;
   enc.bp_right2=0;
 
-  /* options*/
+  /* options */
   enc.noLP=noLP;
   enc.verbose_lvl=verbosity_level;
   enc.first=0;
   enc.shift=shifts;
 
-  /* degeneracy*/
+  /* degeneracy */
   enc.begin_unpr=0;
   enc.begin_pr=0;
   enc.end_unpr=0;
   enc.end_pr=0;
   enc.current_en=0;
 
-  // function
+  /* function */
   enc.funct=NULL;
 
   int i;
@@ -838,26 +831,26 @@ move_first( char *string,
   enc.s0 = s;
   enc.s1 = s1;
 
-  /* moves*/
+  /* moves */
   enc.bp_left=0;
   enc.bp_right=0;
   enc.bp_left2=0;
   enc.bp_right2=0;
 
-  /* options*/
+  /* options */
   enc.noLP=noLP;
   enc.verbose_lvl=verbosity_level;
   enc.first=1;
   enc.shift=shifts;
 
-  /* degeneracy*/
+  /* degeneracy */
   enc.begin_unpr=0;
   enc.begin_pr=0;
   enc.end_unpr=0;
   enc.end_pr=0;
   enc.current_en=0;
 
-  // function
+  /* function */
   enc.funct=NULL;
 
   int i;
@@ -892,29 +885,29 @@ move_adaptive(char *string,
   enc.s0 = s;
   enc.s1 = s1;
 
-  /* moves*/
+  /* moves */
   enc.bp_left=0;
   enc.bp_right=0;
   enc.bp_left2=0;
   enc.bp_right2=0;
 
-  /* options*/
+  /* options */
   enc.noLP=0;
   enc.verbose_lvl=verbosity_level;
   enc.first=1;
   enc.shift=0;
 
-  /* degeneracy*/
+  /* degeneracy */
   enc.begin_unpr=0;
   enc.begin_pr=0;
   enc.end_unpr=0;
   enc.end_pr=0;
   enc.current_en=0;
 
-  // function
+  /* function */
   enc.funct=NULL;
 
-  // allocate memory for moves
+  /* allocate memory for moves */
   enc.moves_from = (int*) space(ptable[0]*ptable[0]*sizeof(int));
   enc.moves_to = (int*) space(ptable[0]*ptable[0]*sizeof(int));
 
@@ -976,26 +969,26 @@ browse_neighs_pt( char *string,
   enc.s0 = s;
   enc.s1 = s1;
 
-  /* moves*/
+  /* moves */
   enc.bp_left=0;
   enc.bp_right=0;
   enc.bp_left2=0;
   enc.bp_right2=0;
 
-  /* options*/
+  /* options */
   enc.noLP=noLP;
   enc.verbose_lvl=verbosity_level;
   enc.first=1;
   enc.shift=shifts;
 
-  /* degeneracy*/
+  /* degeneracy */
   enc.begin_unpr=0;
   enc.begin_pr=0;
   enc.end_unpr=0;
   enc.end_pr=0;
   enc.current_en=0;
 
-  // function
+  /* function */
   enc.funct=funct;
 
   int i;
@@ -1014,7 +1007,7 @@ browse_neighs_pt( char *string,
   return str.energy;
 }
 
-/* printf*/
+/* printf */
 PUBLIC void
 print_stren(FILE *out, struct_en *str) {
   print_str(out, str->structure);
@@ -1033,35 +1026,35 @@ print_str(FILE *out, short *str) {
 
 
 #ifdef TEST_MOVESET
-// sample usage:
+/* sample usage: */
 int main() {
   char seq[20] = "ACCCCCCTCTGTAGGGGGA";
   char str[20] = ".((.(.........).)).";
 
-  // move to the local minimum and display it
+  /* move to the local minimum and display it */
   int energy = move_standard(seq, str, GRADIENT, 0, 0, 0);
   fprintf(stdout, "%s %6.2f\n\n", str, energy/100.0);
 
-  //now create an array of every structure in neighbourhood of str structure
+  /* now create an array of every structure in neighbourhood of str structure */
   struct_en *list = NULL;
   int list_length = 0;
 
   int get_list(struct_en *new_one, struct_en *old_one)
   {
-    // enlarge the list
+    /* enlarge the list */
     list_length++;
     list = (struct_en*) realloc(list, list_length*sizeof(struct_en));
 
-    // copy the structure
+    /* copy the structure */
     list[list_length-1].energy = new_one->energy;
     list[list_length-1].structure = allocopy(new_one->structure);
 
-    // we want to continue -> return 0
+    /* we want to continue -> return 0 */
     return 0;
   }
   browse_neighs(seq, str, 0, 0, 0, get_list);
 
-  // print them and free the memory:
+  /* print them and free the memory: */
   int i;
   for (i=0; i<list_length; i++) {
     print_stren(stdout, &list[i]);
